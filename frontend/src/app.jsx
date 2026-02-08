3535d0b86023dd2cc0f09d9295168abe11b8cee6
work-akshay05 <akshaykumar_23ee022@dtu.ac.in>
2026-02-08 18:27:54 +0530

    final url added

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -14,7 +14,7 @@ const Card = ({note}) => {
         <h1 className='title'>{note.title}</h1>
         <p className='description'>{note.description}</p>
         <button onClick={()=>{
-          axios.delete(`http://localhost:3000/api/notes/${note._id}`)
+          axios.delete(`https://crud-app-kxxw.onrender.com/api/notes/${note._id}`)
           .then((res)=>{
             console.log(res);
           })
@@ -24,10 +24,10 @@ const Card = ({note}) => {
         {showInput && 
           <form onSubmit={(e) => {
             e.preventDefault();
-            axios.patch(`http://localhost:3000/api/notes/${note._id}`, {description:des})
+            axios.patch(`https://crud-app-kxxw.onrender.com/api/notes/${note._id}`, {description:des})
             .then((res) => {
               console.log(res);
-              axios.get('http://localhost:3000/api/notes')
+              axios.get('https://crud-app-kxxw.onrender.com/api/notes')
                 .then((res)=>{
                   console.log(res.data.notes);
                 }
@@ -57,7 +57,7 @@ const App = () => {
     const [description,setDescription]=useState("");
 
     const fetchData=()=>{
-      axios.get('http://localhost:3000/api/notes')
+      axios.get('https://crud-app-kxxw.onrender.com/api/notes')
         .then((res)=>{
           console.log(res.data.notes);
           setArr(res.data.notes);
@@ -71,7 +71,7 @@ const App = () => {
     const submitHandler=(e)=>{
       e.preventDefault();
       console.log(title,description);
-      axios.post('http://localhost:3000/api/notes',{title,description})
+      axios.post('https://crud-app-kxxw.onrender.com/api/notes',{title,description})
       .then((res)=>{
         console.log(res)
       })
